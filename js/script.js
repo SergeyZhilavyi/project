45f032e5e96a312061bc034b33e60aca32203487
Sergey <mega.vercetty@mail.ru>
2020-10-18 21:07:48 +0300

    Функции 1

--- js/script.js
+++ js/script.js
@@ -1,112 +1,14 @@
 "use strict";
 
-/*let number = 4;  //Занятие перемееные
-const trieDripperLive = 2;
-number = 8;
-console.log(number);
+/*function showFirstMessage() {
+    console.log("Hello world!");
+}
 
-const obj = {
-    a: 50
-};
+showFirstMessage();*/
 
-obj.a = 10;
+//Второй вариант 
+function showFirstMessage(text) {
+    console.log(text);
+}
 
-console.log(obj);*/   // Конец занятие переменные
-
-/*console.log(4/0);  // Занятие объекты
-console.log('strict' * 9);
-
-let jove;
-console.log(jove);
-
-const obj = {
-    name: "John",
-    age: 21,
-    isMarried: false
-};
-console.log(obj.age);
-
-let arr = ['basket.png', 'roger', 1, [], {}];
-console.log(arr[2]);*/   //Конец занятия Объекты
-
-//const result = confirm("Are you here?"); //Начало занятия Простое общение с пользователем
-//console.log(result);
-
-//const answer = prompt("Вам есть 18?", "");
-//const answer = +prompt("Вам есть 18?", "");
-//console.log(answer);
-//console.log(typeof(answer));
-
-/*const answer = +prompt("Вам есть 18?", "");
-console.log(answer + 5); */
-
-/*const answers = [];
-//answers [0] = prompt('Как ваше имя ?', "");
-//answers [1] = prompt('Как ваша фамилия ?', "");
-//answers [2] = prompt('Сколько вам лет ?', "");
-
-//document.write(answers);
-console.log(typeof(answers)); */ //Конец занятия Простое общение с пользователем 
-
-/*const category = 'toys'; // Интерполяция ES6 начало
-console.log(`https://chrome.com/${category}/5`); */
-
-/*const user = "Sergo";
-alert(`Привет, ${user}`); */ //Конец Интерполяция ES6
-
-/*let incr = 10, //Операторы в JS oo
-    decr = 10;
-    //++incr;
-    //--decr;
-console.log(incr++);
-console.log(decr--);
-
-console.log(5%2);
-
-console.log(2*4 == 8);
-console.log(2*4 === '8');
-
-console.log(2 + 2 * 2 !== 6); // false
-console.log(2 + 2 * 2 !== '6'); // true т.к 6-ка справа это строка*/ //Конец операторы JS
-
-//Условия
-
-/*const num = 50;
-
-switch (num) {
-    case 48:
-        console.log('Неверно');
-        break;
-    case 80:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('Отлично !');
-        break;
-    default:
-        console.log('Не волнуйся.Всё получится!');
-        break;
-
-} */ //Конец разбора Условий
-
-/*let num = 50;
-/*while (num < 55) {
-    console.log(num);
-    num++;
-}*/
-
- /* for (let i = 1; i < 8; i++) {
-    console.log(num);
-    num++;
-} */
-
-let i = 0;
-let n = 0; 
-while (i < 5) {
-  i++;
-  if (i == 3) {
-    continue;
-  }
-  n += i; //Выедет 1,3,7,12
-  
-}  
+showFirstMessage("Hello world!");
