98f8f3f1623d26de6c2366a67c97043d48aede57
Sergey <mega.vercetty@mail.ru>
2020-10-18 23:10:54 +0300

    Функция 3

--- js/script.js
+++ js/script.js
@@ -13,11 +13,44 @@ showFirstMessage();*/
 
 showFirstMessage("Hello world!");*/
 
-let num = 20;
+/*let num = 20;
 function showFirstMessage(text) {
     console.log(text);
     num = 10;
 }
 
 showFirstMessage("Hello world!");
-console.log(num);
+console.log(num);*/
+
+/*function calc(a,b) {
+
+    return (a + b);
+}
+
+console.log(calc(4,3));
+console.log(calc(5,6));
+console.log(calc(10,6));
+
+function ret() {
+    let num = 50;
+
+    //Много кода :)
+    return num ;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+//Function expression
+const logger = function() {
+
+    console.log("Hello");
+
+};
+
+logger(); //Hello*/
+
+const calc = ( a,b ) => {return a + b};
+console.log(calc(4,3));
+console.log(calc(5,6));
+console.log(calc(10,6));
