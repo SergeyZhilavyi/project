b273508614eda7365399c23c06a87dec89ecd5ea
Sergey <mega.vercetty@mail.ru>
2020-10-18 22:13:51 +0300

    Функции 2

--- js/script.js
+++ js/script.js
@@ -7,8 +7,17 @@
 showFirstMessage();*/
 
 //Второй вариант 
-function showFirstMessage(text) {
+/*function showFirstMessage(text) {
     console.log(text);
 }
 
+showFirstMessage("Hello world!");*/
+
+let num = 20;
+function showFirstMessage(text) {
+    console.log(text);
+    num = 10;
+}
+
 showFirstMessage("Hello world!");
+console.log(num);
