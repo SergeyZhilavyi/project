e9c6d335ecb00ee577a3d554d04447a8544da044
Sergey <mega.vercetty@mail.ru>
2020-10-26 23:12:57 +0300

    Callback - функции

--- js/script.js
+++ js/script.js
@@ -1,69 +1,45 @@
 "use strict";
 
-/*const str = "test";
-console.log(str.length); //4
+function first() {
+  // Какие-то действия
+  setTimeout(function() {
+    console.log(1);
+  }, 500);
+}
 
-const arr = [1 ,2 ,4 ,5 ,7];
-console.log(arr.length); //5 */
+function second() {
+  console.log(2);
+}
 
-/*const str = "test";
-const arr = [1 ,2 ,4 ,5 ,7];
-console.log(str[3]); //Выведет t  */
+first();
+second();
+//Выведет: 2,1
 
-const str = "test";
-console.log(str.toUpperCase());
-//console.log(str.toLowerCase());
-console.log(str); //TEST ,test
+function learnJS(lang, callback) {
+  console.log(`Я учу: ${lang}`); //сначала эта
+  callback(); //Потом эта
+}
 
-//const fruit = "Some fruit";
-//console.log(fruit.indexOf("fruit")); // Выведет 5
+learnJS('JavaScript', function(){
 
-const fruit = "Some fruit";
-console.log(fruit.indexOf("q")); // Выведет -1
+  console.log('Я прошёл этот урок !');
 
- // logg = "Hello world";
- //console.log(logg.slice(4, 8)); // Выведет o wo
+}); //Выведет: Я учу: JavaScript
+//Я прошёл этот урок ! 
 
- //const logg = "Hello world";
- //console.log(logg.slice(2, 10)); // Выведет llo worl
 
- //const logg = "Hello world";
- //console.log(logg.slice(2, 11)); // Выведет llo world 
 
- //const logg = "Hello world";
- //console.log(logg.slice(4)); // Выведет o world
+function learn(lang, callback) {
+  console.log(`Я учу: ${lang}`); //сначала эта
+  callback(); //Потом эта
+}
 
- const logg = "Hello world";
- console.log(logg.slice(-4, -2)); // Выведет or
+function done() {
 
- //const gonta = "Hello gonta";
- //console.log(gonta.substr(6, 4)); //Выведет gont
+  console.log('Я прошёл этот урок !');
+}
 
- const gonta = "Hello gonta";
- console.log(gonta.substr(6, 3)); //Выведет gon
+learn('JavaScript', done);
 
- //const num = 24.72;
- //console.log(Math.round(num)); //25
-
- const num = 24.2;
- console.log(Math.round(num)); //24
-
- // test = "14.4px";
- //console.log(parseInt(test)); //14 
-
- const test = "14.4px";
- console.log(parseFloat(test)); //14.4
-
- function createCounter() {
-       let counter = 0;
-   const myFunction = function() { 
-         counter = counter + 3;
-         return counter;
-       };
-       return myFunction;
-     }
-     const increment = createCounter();
-    const c1 = increment();
-    const c2 = increment();
-    const c3 = increment();
-    console.log('example increment', c1, c2, c3);
+ //Выведет: Я учу: JavaScript
+//Я прошёл этот урок ! 
