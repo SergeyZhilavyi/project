a3d13cfc6c2961930fc62ad49a8a31b59630c5c7
Sergey <mega.vercetty@mail.ru>
2020-10-27 23:48:04 +0300

    Объекты,диструктуризация объектов ES6

--- js/script.js
+++ js/script.js
@@ -1,45 +1,47 @@
 "use strict";
 
-function first() {
-  // Какие-то действия
-  setTimeout(function() {
-    console.log(1);
-  }, 500);
+const options = {
+  time: 'winter',
+  lenght: 5,
+  climate: 'is cold',
+  features: {
+     ice: 'slippery',
+     snow: 'white'
+  }
+
+};
+//С помощью метода Object.keys узнаём длину объекта
+console.log(Object.keys(options).length); //4
+
+//console.log(options.features.ice); //slippery
+//Удаляем одно свойство из объекта
+/*delete options.features.snow;
+console.log(options);*/
+
+//Перебераем свойства объекта
+/*for (let key in options) {
+  console.log(`Свойство ${key} имеет значение ${options[key]}`);
+}*/
+//Выведет:
+/*Свойство time имеет значение winter
+Свойство lenght имеет значение 5
+Свойство climate имеет значение is cold
+Свойство features имеет значение [object Object]*/
+
+//Решаем проблему корректного вывода вложенного объекта
+
+for (let key in options) {
+  if (typeof(options[key]) === 'object') {
+    for (let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    }
+  } else {
+  console.log(`Свойство ${key} имеет значение ${options[key]}`);
+  }
 }
-
-function second() {
-  console.log(2);
-}
-
-first();
-second();
-//Выведет: 2,1
-
-function learnJS(lang, callback) {
-  console.log(`Я учу: ${lang}`); //сначала эта
-  callback(); //Потом эта
-}
-
-learnJS('JavaScript', function(){
-
-  console.log('Я прошёл этот урок !');
-
-}); //Выведет: Я учу: JavaScript
-//Я прошёл этот урок ! 
-
-
-
-function learn(lang, callback) {
-  console.log(`Я учу: ${lang}`); //сначала эта
-  callback(); //Потом эта
-}
-
-function done() {
-
-  console.log('Я прошёл этот урок !');
-}
-
-learn('JavaScript', done);
-
- //Выведет: Я учу: JavaScript
-//Я прошёл этот урок ! 
+//Выведет:
+/*Свойство time имеет значение winter
+Свойство lenght имеет значение 5
+Свойство climate имеет значение is cold
+Свойство ice имеет значение slippery
+Свойство snow имеет значение white*/ 
